3aa960d76d25c242b8cf654e57e4390057d8bf15
takayasu.nasu <taka.beckham@gmail.com>
2014-03-22 17:49:42 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+Arsenal
+===
+
+#### Outline
+
+- グループウェア
+
+#### Features
+
+- 認証
+- スケジュール
+- todo
+
+#### Versions
+
+- 0.0.1 [2014/03/21]
+
+#### License
+
+- Copyright (c) 2014 kubota nasu etc
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Arsenal::Application.config.secret_key_base = 'b9962078401f530716afcebab66ea46495ddd34f9a10a8d2a7e3e5a63ab57b570d8feeb224fe739abf9250b5ec8fd4813d0b1e2b2502abd2684d11ba9f4c222d'
