952a9cf3212a87d763bdff01a63b54174ba3b6c6
takayasu.nasu <taka.beckham@gmail.com>
2014-05-04 23:35:21 +0900

    googlemap上のユーザーの勤務地にアイコンを表示

--- app/assets/javascripts/angular/controllers/staff_all_ctrl.js
+++ app/assets/javascripts/angular/controllers/staff_all_ctrl.js
@@ -2,7 +2,44 @@ var app = angular.module('Arsenal');
 
 app.controller('StaffAllCtrl', [
   '$scope', 'StaffAll', function($scope, StaffAll) {
-    $scope.staffs = StaffAll.query();
+    var loan_company_data = {};
+    var markerData = [];
+    $scope.staffs = StaffAll.query(function (response) {
+      angular.forEach(response, function (item) {
+        if (item.loan_company) {
+            loan_company_data.full_name = item.full_name;
+            loan_company_data.mugshot_url = item.mugshot_url;
+            loan_company_data.loan_company_name = item.loan_company.name;
+            loan_company_data.loan_company_address = item.loan_company.address;
+            markerData.push(loan_company_data);
+        };
+      });
+      console.log(markerData[0].loan_company_address);
+
+      var geocoder = new google.maps.Geocoder();
+      geocoder.geocode({
+        address: markerData[0].loan_company_address
+    }, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            for (var i in results) {
+                 if (results[i].geometry) {
+                    var latlng = results[i].geometry.location;
+                    console.log(latlng.lat());
+
+                    $scope.myMarkers = [
+                    {
+                        latitude:  latlng.lat(),
+                        longitude: latlng.lng(),
+                        mugshot_url: markerData[0].mugshot_url
+                    }
+                    ];
+                 }
+            }
+        }
+    });
+
+    });
+
     console.log($scope.staffs);
 
     $scope.map = {
@@ -10,7 +47,7 @@ app.controller('StaffAllCtrl', [
     		latitude: 35.657153,
     		longitude: 139.696332
     	},
-    	zoom: 15
+    	zoom: 13
     }
   }
 ]);
