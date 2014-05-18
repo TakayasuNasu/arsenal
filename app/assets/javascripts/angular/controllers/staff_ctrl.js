63b09961e7abf0dd287277c286679711fb3fd566
takayasu.nasu <taka.beckham@gmail.com>
2014-05-18 10:56:33 +0900

    mod 一括登録にて通信中のモーダルを表示

--- app/assets/javascripts/angular/controllers/staff_ctrl.js
+++ app/assets/javascripts/angular/controllers/staff_ctrl.js
@@ -1,8 +1,24 @@
 var app = angular.module('Arsenal');
 
 app.controller('StaffsCtrl', [
-  '$scope', 'Staff', function($scope, Staff) {
-    $scope.staffs = Staff.query();
+  '$scope', '$modal','Staff', function($scope, $modal, Staff) {
+
+    var modalInstance = $modal.open({
+      templateUrl:"progress.html",
+      backdrop:"static",
+      keyboard:false
+    });
+
+    $scope.staffs = Staff.query(
+      {},
+      function(){
+        modalInstance.close();
+      },
+      function(){
+        modalInstance.close();
+        $scope.data = {result: "通信エラー"}
+      }
+    );
 
     $scope.checkAll = function(){
     	if (document.staffInfo.allChacke.checked) {
