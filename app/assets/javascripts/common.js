fbd8a908c2d42e7d495488a1bc47e91c0edd2edf
takayasu.nasu <taka.beckham@gmail.com>
2014-07-12 19:25:52 +0900

    mod js, scss, home/index

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -53,3 +53,25 @@ $(function() {
 	});
 });
 
+$(function () {
+	var topBtn = $('#page-top');
+	topBtn.hide();
+
+	//スクロールが500に達したらボタン表示
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 500) {
+			topBtn.fadeIn();
+		} else {
+			topBtn.fadeOut();
+		}
+	});
+
+	//スクロールしてトップ
+	topBtn.click(function () {
+		$('body,html').animate({
+			scrollTop: 5
+		},500);
+	});
+	return false;
+});
+
